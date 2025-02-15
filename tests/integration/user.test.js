02cc316f3fedfdd663b8b531ec7fc6d66213c4d7
waleed21121 <wa581509@gmail.com>
2025-02-15 22:30:50 +0200

    Test no users in our DB

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -1,9 +1,17 @@
 require('../../config/DBconfig');
 const userModel = require('../../models/user.model');
+const userDoc = require('./userDoc');
 
-beforeEach(() => {
-    userModel.User.deleteMany({});
+beforeEach(async () => {
+    await userModel.User.deleteMany({});
 })
-afterAll(() => {
-    userModel.User.deleteMany({});
+afterAll(async () => {
+    await userModel.User.deleteMany({});
+})
+
+describe("get user", () => {
+    it('should return no user', async () => {
+        const users = await userModel.getQueryObject();
+        expect(users.length).toBe(0);
+    })
 })
