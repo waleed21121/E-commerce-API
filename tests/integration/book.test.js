87fd1ae3cad9674004c2c4cac0bcfd5fa3997ea5
waleed21121 <wa581509@gmail.com>
2025-02-15 12:46:52 +0200

    feat: delete all the products before each test case

--- tests/integration/book.test.js
+++ tests/integration/book.test.js
@@ -2,6 +2,10 @@ require('../../config/DBconfig');
 const productModel = require('../../models/product.model');
 require('../../models/category.model');
 
+beforeEach(async () => {
+    productModel.Product.deleteMany({});
+})
+
 describe("get products", () => {
     it('should return empty array', async () => {
         const products = await productModel.getQueryObject();
