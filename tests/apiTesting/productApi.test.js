b90e0353ffd192c8a640a2ee83c2f7dc345593ae
waleed21121 <wa581509@gmail.com>
2025-02-17 12:35:42 +0200

    Test Get product by its id route

--- tests/apiTesting/productApi.test.js
+++ tests/apiTesting/productApi.test.js
@@ -3,7 +3,7 @@ const server = require('../../app');
 const mongoose = require('../../config/DBconfig');
 const {Product} = require('../../models/product.model');
 const productDoc = require('../integration/productDoc');
-
+const {generateToken} = require('../../features/JWT');
 beforeEach(async function () {
     await Product.deleteMany({});
 })
@@ -21,5 +21,18 @@ describe('get books', () => {
         expect(response.body.status).toBe('success');
         expect(response.body.data.length).toBe(1);
         expect(response.body.data[0].name).toBe(productDoc.name);
+    })
+
+    it('should return the book by its id', async () => {
+
+        const product = await Product.create(productDoc);
+
+        const token = generateToken('test@test.com');
+        const response = await request(server)
+            .get(`/api/products/${product._id}`);
+        
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe('success');
+        expect(response.body.data).toMatchObject({name: productDoc.name, description: productDoc.description});
     })
 })
