df4c1ecea5cea4475b5c9d8e3f29559b9d532fd3
waleed21121 <wa581509@gmail.com>
2025-02-17 17:57:00 +0200

    Test get all users route

--- tests/apiTesting/userApi.test.js
+++ tests/apiTesting/userApi.test.js
@@ -13,3 +13,15 @@ afterAll(async function () {
     await User.deleteMany({});
     await mongoose.disconnect();
 });
+
+describe('Testing user', () => {
+    describe('Get all users', () => {
+        it('should return one user', async () => {
+            const user = await User.create(userDoc);
+            const token = generateToken('test@test.com');
+            const response = await request(server).get('/api/users').set('Authorization', `Bearer ${token}`);
+            expect(response.status).toBe(200);
+            expect(response.body.data[0]).toMatchObject({email: user.email});
+        })
+    })
+})
