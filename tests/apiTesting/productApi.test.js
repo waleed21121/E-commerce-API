be324f7094fadf8f3671bfbd8d6cc1b7edf5ec71
waleed21121 <wa581509@gmail.com>
2025-02-17 11:39:04 +0200

    Test get all products route

---
+++ tests/apiTesting/productApi.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const server = require('../../app');
+const mongoose = require('../../config/DBconfig');
+const {Product} = require('../../models/product.model');
+const productDoc = require('../integration/productDoc');
+
+describe('get books', () => {
+    it('should return one book', async () => {
+        await Product.create(productDoc);
+        const response = await request(server).get('/api/products/');
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe('success');
+        expect(response.body.data.length).toBe(1);
+        expect(response.body.data[0].name).toBe(productDoc.name);
+    })
+})
